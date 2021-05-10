5083caa2672ad01a59e9b80932748e3f31008ef4
hanzife <hanzife123@gmail.com>
2021-05-10 15:52:41 +0000

    user registration done

--- src/main/java/com/devglan/springboothibernatelogin/controller/UserController.java
+++ src/main/java/com/devglan/springboothibernatelogin/controller/UserController.java
@@ -5,12 +5,10 @@ import com.devglan.springboothibernatelogin.dto.LoginDto;
 import com.devglan.springboothibernatelogin.dto.SignUpDto;
 import com.devglan.springboothibernatelogin.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("http://localhost:4200") //Adapt port to agular server to let angular have access to backend
 @RequestMapping("/users")
 public class UserController {
 
